61f21126f1f46eb8553e59c7c8195d159b76d105
RoLoDeXx <dexxrolo@gmail.com>
2019-07-03 19:50:52 +0530

    Updates

---
+++ precache-manifest.59c4f7fee1a6e3a9c4bae0995050f7d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8751889790824c5224592cf38b357fb",
+    "url": "/sam-react-rps/index.html"
+  },
+  {
+    "revision": "b2b5bc462e6f9c4c5a8b",
+    "url": "/sam-react-rps/static/css/main.7efbdb8d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/sam-react-rps/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b2b5bc462e6f9c4c5a8b",
+    "url": "/sam-react-rps/static/js/main.0787d3b6.chunk.js"
+  },
+  {
+    "revision": "c24d8c5c57c930df9f4d",
+    "url": "/sam-react-rps/static/js/runtime~main.fe972661.js"
+  }
+]);
